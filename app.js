c77c540c6aaed8e62c899562db6c17805331f675
Robert <rbaxter.08@gmail.com>
2017-08-02 20:36:17 -0400

    testing oauth

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 
+const TOKEN = 'xoxp-26503446353-214024588853-214047871506-82c88ebdbc46753600ea689e9084991e';
+const TOKEN_BOT ='xoxb-214939346278-FWmSHRdjF1rw91KU0PJAJmQd';
+const SCOPE = 'client';
+const CLIENT_ID = '26503446353.214933754470';
+const CLIENT_SECRET = '49b8fea646167daefd4f535084abd45f';
+const REDIRECT = 'https://baxter-bot.herokuapp.com/';
+const VERIFICATION_TOKEN = 'tkPGmSRMWtxlWjw1rJZ0PsyP';
+
 let app = express();
 let port = process.env.PORT || 1337;
 
@@ -34,14 +42,14 @@ app.get('/oauth', (req, res, next) => {
 	console.log(req.body);
 });
 
-const TOKEN = 'xoxp-26503446353-214024588853-214047871506-82c88ebdbc46753600ea689e9084991e';
-const TOKEN_BOT ='xoxb-214939346278-FWmSHRdjF1rw91KU0PJAJmQd';
-const SCOPE = 'rtm:stream,client';
-const CLIENT_ID = '26503446353.214933754470';
-const CLIENT_SECRET = '49b8fea646167daefd4f535084abd45f';
-const REDIRECT = 'https://baxter-bot.herokuapp.com/';
-const VERIFICATION_TOKEN = 'tkPGmSRMWtxlWjw1rJZ0PsyP';
+function authorizeApp() {
+	let url = `https://slack.com/oauth/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT}`;
+	request.get(url, (err, response, data) => {
+		console.log(response);
+	});
+};
 
+authorizeApp();
 
 // request.post(`https://slack.com/api/rtm.connect?token=${token}&scope=${scope}`, {}, (err, response, data) => {
 // 	console.log(data);
